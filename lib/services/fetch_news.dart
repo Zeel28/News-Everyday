f894acc5eaca1c071885d4d68372cd283cf2995c
Zeel Bhanderi <zeelbhanderi28@gmail.com>
2023-03-10 19:17:47 +0530

    add fetch data every refresh

--- lib/services/fetch_news.dart
+++ lib/services/fetch_news.dart
@@ -9,41 +9,29 @@ class FetchNews{
 
   static List sourcesId = [
     "abc-news",
-
     "bbc-news",
     "bbc-sport",
-
     "business-insider",
-
     "engadget",
     "entertainment-weekly",
     "espn",
     "espn-cric-info",
     "financial-post",
-
     "fox-news",
     "fox-sports",
     "globo",
     "google-news",
     "google-news-in",
-
     "medical-news-today",
-
     "national-geographic",
-
     "news24",
     "new-scientist",
-
     "new-york-magazine",
     "next-big-future",
-
     "techcrunch",
     "techradar",
-
     "the-hindu",
-
     "the-wall-street-journal",
-
     "the-washington-times",
     "time",
     "usa-today",
@@ -62,5 +50,23 @@ class FetchNews{
 
     return Articles.fromJson(myArticle);
 
+  }
+}
+class NewsApi {
+  static const String _apiKey = '9f6d210f572a43c2892e84040a32e190';
+  static const String _baseUrl = 'https://newsapi.org/v2';
+
+  static Future<List<Articles>> fetchTopHeadlines(String sourceId) async {
+    final response = await http.get(Uri.parse('$_baseUrl/top-headlines?sources=$sourceId&apiKey=$_apiKey'));
+
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body);
+      final List<dynamic> articlesJson = json['articles'];
+      return articlesJson
+          .map((articleJson) => Articles.fromJson(articleJson))
+          .toList();
+    } else {
+      throw Exception('Failed to fetch top headlines.');
+    }
   }
 }
--- lib/ui/Screens/feed/newsfeed.dart
+++ lib/ui/Screens/feed/newsfeed.dart
@@ -1,11 +1,11 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 import 'package:news_everyday/model/article_model.dart';
 import 'package:news_everyday/ui/Screens/feed/widget/NewsContainer.dart';
 import 'package:news_everyday/ui/Screens/feed/widget/my_floating_actionbutton.dart';
 import 'package:news_everyday/utils/message.dart';
-import 'package:share_plus/share_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
-import '../../../services/api_service.dart';
 import '../../../services/fetch_news.dart';
 import '../../theme/colors.dart';
 
@@ -17,43 +17,37 @@ class NewsFeed extends StatefulWidget {
 }
 
 class _NewsFeedState extends State<NewsFeed> {
-  List<Articles> newsFeedArticle = [];
-  bool _isLoading = true;
-  late String? _errorMessage;
+
   int _currentPage = 0;
+  final _random = Random();
+  List<Articles> newsFeedArticle = [];
 
   @override
   void initState() {
     super.initState();
-    _loadNews();
+    _fetchArticles();
   }
 
-  final queryParameters = {
-    'q': "google-news",
-    'sortBy': "popularity,publishedAt",
-    'language': 'en',
-    'apiKey': '3f1a0e1381c74be2ab644cf747bfad83',
-  };
-
-  Future<void> _loadNews() async {
-    try {
-      final article = await ApiService.searchNews(queryParameters);
-      setState(() {
-        newsFeedArticle = article;
-        _isLoading = false;
-        _errorMessage = null;
-      });
-    } catch (e) {
-      setState(() {
-        newsFeedArticle = [];
-        _isLoading = false;
-        _errorMessage = e.toString();
-      });
-    }
+  Future<void> _fetchArticles() async {
+    final sourceId = FetchNews.sourcesId[_random.nextInt(FetchNews.sourcesId.length)];
+    final articles = await NewsApi.fetchTopHeadlines(sourceId);
+    setState(() {
+      newsFeedArticle = articles;
+    });
+  }
+  Future<void> _handleRefresh() async {
+    return await _fetchArticles();
   }
 
   @override
   Widget build(BuildContext context) {
+
+    if (newsFeedArticle.isEmpty) {
+      return Center(
+        child: MessageDialog().progressIndicator(context)
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
@@ -73,7 +67,7 @@ class _NewsFeedState extends State<NewsFeed> {
                   String email =
                       Uri.encodeComponent("info.newseveryday@gmail.com");
                   String subject =
-                      Uri.encodeComponent("Report a News ${newsFeedArticle[_currentPage].title}");
+                      Uri.encodeComponent("Report a News *${newsFeedArticle[_currentPage].title}*");
                   Uri mail = Uri.parse("mailto:$email?subject=$subject");
                   if (await launchUrl(mail)) {
                     //email app opened
@@ -104,27 +98,30 @@ class _NewsFeedState extends State<NewsFeed> {
           ),
         ],
       ),
-      body: PageView.builder(
-        controller: PageController(initialPage: 0),
-        scrollDirection: Axis.vertical,
-        onPageChanged: (value) {
-          setState(() {
-            _isLoading = true;
-            _currentPage = value;
-          });
-          _loadNews();
-        },
-        itemBuilder: (context, index) {
-          if (_isLoading) {
-            return Center(child: MessageDialog().progressIndicator(context));
-          } else {
-            return NewsContainer(
-              article: newsFeedArticle[index]
-            );
-          }
-        },
+      body: LiquidPullToRefresh(
+        onRefresh: _handleRefresh,
+        color: primaryColor,
+        height: 150,
+        animSpeedFactor: 2,
+        backgroundColor: primaryLightColor,
+        showChildOpacityTransition: false,
+        child: PageView.builder(
+          physics: const BouncingScrollPhysics(),
+          itemCount: newsFeedArticle.length,
+          scrollDirection: Axis.vertical,
+          onPageChanged: (value) {
+            setState(() {
+              _currentPage = value;
+            });
+          },
+          itemBuilder: (context, index) {
+              return NewsContainer(
+                article: newsFeedArticle[index]
+              );
+            }
+        ),
       ),
-      floatingActionButton: _isLoading ? null  : MyFloatingActionButton(article: newsFeedArticle[_currentPage],)
+      floatingActionButton: MyFloatingActionButton(article: newsFeedArticle[_currentPage],)
     );
   }
 }
